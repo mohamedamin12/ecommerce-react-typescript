9b7b17f876b1cdbebdf0a1ff7799ae52bff51f80
mohamedamin12 <aboamin909080@gmail.com>
2025-07-14 12:04:15 +0300

    Update project configuration and dependencies; rename title in index.html, remove unused tsconfig.app.json, and enhance tsconfig.json with path aliases. Added vite-plugin-svgr and vite-tsconfig-paths for improved asset handling and path resolution.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import AppRouter from "@routes/AppRouter";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "@styles/global.css";
 
 
 createRoot(document.getElementById('root')!).render(
-  <div>hello</div>
+  <div>
+    <AppRouter />
+  </div>
 )
